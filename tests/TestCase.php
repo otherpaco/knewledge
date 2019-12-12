bbcc11a65f8acd7f7db730524cc8d8e844c8d857
Matthias Niess <mniess@gmail.com>
2019-12-12 14:02:07 +0100

    testing preparations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,18 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withoutExceptionHandling();
+    }
+
+    protected function signIn($user = null)
+    {
+        $user = $user ?: factory(\App\User::class)->create();
+        $this->actingAs($user);
+        return $user;
+    }
 }
