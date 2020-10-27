9773d6ef32dce7e0bfa1d2f2451039fbb1faa312
Matthias Niess <mniess@gmail.com>
2020-10-27 21:23:00 +0100

    prepare tailwind setup for tailwind 2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/custom-forms')
-  ]
-}
+    future: {
+        removeDeprecatedGapUtilities: true,
+        purgeLayersByDefault: true,
+        defaultLineHeights: true,
+        standardFontWeights: true
+    },
+    purge: [
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {},
+    },
+
+    variants: {
+        opacity: ['responsive', 'hover', 'focus', 'disabled'],
+    },
+
+    plugins: [
+        require('@tailwindcss/custom-forms'),
+        require('@tailwindcss/ui'),
+    ],
+
+};
