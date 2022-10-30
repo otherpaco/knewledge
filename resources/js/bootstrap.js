14577d06d171d31ee2181bc630c088d419c885c6
Matthias Niess <mniess@gmail.com>
2022-10-30 16:22:31 +0100

    switch from mix to vite

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,12 +1,3 @@
-// import _ from 'lodash';
-// window._ = _;
-
-// import axios from 'axios';
-// window.axios = axios;
-
-// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-
 // import Echo from 'laravel-echo';
 
 // import Pusher from 'pusher-js';
